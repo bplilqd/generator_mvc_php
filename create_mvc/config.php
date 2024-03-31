c0508b13135f361eb17ab50e03da0928d9a93ba3
Ivan Gerasimov <bqlilpd@gmail.com>
2024-03-31 19:26:24 +0300

    path change default

--- create_mvc/config.php
+++ create_mvc/config.php
@@ -1,7 +1,7 @@
 <?php
 define("DS", DIRECTORY_SEPARATOR);
 
-$path = __DIR__; // path where need create dirs
+$path = '..'; // path where need create dirs
 
 include 'arr_to_json_for_mvc.php';
 include 'class_mvc.php';
